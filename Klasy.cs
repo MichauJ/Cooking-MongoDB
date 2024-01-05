3aea74d315a08caac2d5f5c8b61defcb5126524a
MichauJ <152602669+MichauJ@users.noreply.github.com>
2024-01-05 10:31:30 +0100

    Update Klasy.cs
    usunięcie klasy "Składnik" dla uproszczenia; utworzenie typu wyliczeniowego enum dla wartości kosztów

--- Klasy.cs
+++ Klasy.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 public class Przepis
 {
     public string Nazwa { get; set; }
-    public List<Skladnik> Skladniki { get; set; }
+    public List<string> Skladniki { get; set; }
     public  List<string> Tagi { get; set; }
     public List<DateTime> DatyPrzygotowania { get; set; }
     public string Zrodlo { get; set; }
@@ -12,21 +12,20 @@ public class Przepis
     public List<string> Uwagi { get; set; }
     public int Ocena { get; set; }
     public string Instrukcja { get; set; }
-    public string Koszt { get; set; }
+    public KosztEnum Koszt { get; set; }
     public int CzasPrzygotowania { get; set; }
 
     public Przepis()
     {
-        Skladniki = new List<Skladnik>();
+        Skladniki = new List<string>();
         Tagi = new List<string>();
         DatyPrzygotowania = new List<DateTime>();
         Uwagi = new List<string>();
     }
-}
-
-public class Skladnik
-{
-    public string Nazwa { get; set; }
-    public double Ilosc { get; set; }
-    public  string Jednostka { get; set; }
+    public enum KosztEnum
+    {
+    Drogi,
+    Umiarkowany,
+    Tani
+    }
 }
