841b59a4a2801f0804de261f9828d5f8964c19c9
MichauJ <152602669+MichauJ@users.noreply.github.com>
2024-01-19 00:47:48 +0100

    Add files via upload

--- Klasy.cs
+++ Klasy.cs
@@ -3,6 +3,12 @@ using MongoDB.Bson;
 using MongoDB.Driver;
 using System;
 using System.Collections.Generic;
+using MongoDB.Bson.IO;
+using Newtonsoft.Json;
+using NewtonsoftJsonConvert = Newtonsoft.Json.JsonConvert;
+using CsvHelper;
+using CsvHelper.Configuration;
+using System.Globalization;
 
 public class Przepis
 {
@@ -11,7 +17,7 @@ public class Przepis
     public string Id { get; set; }
     public string Nazwa { get; set; }
     public List<string> Skladniki { get; set; }
-    public  List<string> Tagi { get; set; }
+    public List<string> Tagi { get; set; }
     public List<DateTime> DatyPrzygotowania { get; set; }
     public string Zrodlo { get; set; }
     public string ZdjeciePath { get; set; }
@@ -73,5 +79,39 @@ public class PrzepisRepository
             .Set(p => p.Ocena, przepis.Ocena);
 
         _przepisyCollection.UpdateOne(filter, update);
+    }
+    public void DodajPrzepisyDoBazy(List<Przepis> przepisy)
+    {
+        _przepisyCollection.InsertMany(przepisy);
+    }
+    public List<Przepis> ImportujJson(string json)
+    {
+        //Parsuje JSON do listy obiektów Przepis
+        List<Przepis> przepisy = NewtonsoftJsonConvert.DeserializeObject<List<Przepis>>(json);
+        
+        return przepisy;
+    }
+    public List<Przepis> PobierzPrzepisyWedługFiltru(string filtrTag)
+    {
+        // Utwórz filtr na podstawie przekazanej nazwy przepisu
+        var filtr = Builders<Przepis>.Filter.Where(p => p.Tagi.Any(tag => tag.ToLower() == filtrTag.ToLower()));
+
+        // Wywołanie zapytania z użyciem filtru
+        var wyniki = _przepisyCollection.Find(filtr).ToList();
+
+        return wyniki;
+    }
+    public void ExportToJson<T>(List<T> data, string filePath)
+    {
+        var json = NewtonsoftJsonConvert.SerializeObject(data, Formatting.Indented);
+        File.WriteAllText(filePath, json);
+    }
+    public void ExportToCsv(List<Przepis> data, string filePath)
+    {
+        using (var writer = new StreamWriter(filePath))
+        using (var csv = new CsvWriter(writer, new CsvConfiguration(CultureInfo.InvariantCulture)))
+        {
+            csv.WriteRecords(data);
+        }
     }
 }
